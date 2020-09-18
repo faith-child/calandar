22ac48ce990d14fff97a652a9b3540c30a065a13
faith-child <fxchild05@gmail.com>
2020-09-18 15:15:52 +0100

    Added style to calander

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Calendar from './calendar';
+import MyCalendar from './calendar';
+
 
 function App() {
   return (
     <div className="App">
-      <Calendar/>
+      <MyCalendar/>
     </div>
   );
 }
