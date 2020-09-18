4793cf83e07dafbf4fb3ac0b4c81cc201b1cf139
faith-child <fxchild05@gmail.com>
2020-09-18 12:14:34 +0100

    Initial commit

---
+++ src/calendar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Calendar extends React.Component{
+    render(){
+        return(
+            <>
+                <h2>Calendar</h2>
+            </>
+        )
+    }
+        
+}
+   
+export default Calendar
