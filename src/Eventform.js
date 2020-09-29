35c7a6eb169ae1b2b2ccb78278aa248d38b677f4
faith-child <fxchild05@gmail.com>
2020-09-29 10:10:21 +0100

    Stored data from form to local storage

--- src/Eventform.js
+++ src/Eventform.js
@@ -4,85 +4,133 @@ import {
     FormControl,
     FormLabel,
     Input,
-    Button,
-    Box
-    
-  } from "@chakra-ui/core";
-class Eventform extends React.Component{
-    constructor(){
+    Button
+
+} from "@chakra-ui/core";
+class Eventform extends React.Component {
+    constructor() {
         super()
-        this.state={
-            name:"",
-            value:"",
-            onChange:""
+        this.state = {
+            event: "",
+            date: "",
+            starttime: "",
+            endtime: ""
+
         }
-        this.handleChange=this.handleChange.bind(this)
-       
-    }
-    handleChange(event){
-        const{name,value}= event.target
-        this.setState({[name]: value})
+        this.onChangeEvent = this.onChangeEvent.bind(this)
+        this.onChangeDate = this.onChangeDate.bind(this)
+        this.onChangestartTime = this.onChangestartTime.bind(this)
+        this.onChangeendTime = this.onChangeendTime.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
     }
 
-    render(props){
-        return(
+    onChangeEvent(e) {
+        this.setState({ event: e.target.value })
+    }
+
+    onChangeDate(e) {
+        this.setState({ date: e.target.value })
+    }
+    onChangestartTime(e) {
+        this.setState({ starttime: e.target.value })
+    }
+    onChangeendTime(e) {
+        this.setState({ endtime: e.target.value })
+    }
+    onSubmit(e) {
+        e.preventDefault()
+        this.setState({
+            event: "",
+            date: "",
+            starttime: "",
+            endtime: ""
+        })
+    }
+
+    componentDidMount() {
+        this.userData = JSON.parse(localStorage.getItem('user'));
+
+        if (localStorage.getItem('user')) {
+            this.setState({
+                event: this.userData.event,
+                date: this.userData.date,
+                startTime: this.userData.startTime,
+                endTime: this.userData.endTime
+            })
+        } else {
+            this.setState({
+                event: "",
+                date: "",
+                starttime: "",
+                endtime: ""
+            })
+        }
+    }
+    componentDidUpdate(nextProps, nextState) {
+        localStorage.setItem('user', JSON.stringify(nextState));
+    }
+
+    render() {
+        return (
             <form>
-                <FormControl p="50px"> 
-                        <FormLabel> Event Name </FormLabel>
-                        <Input 
-                            name= "event"
-                            type = "text"
-                            placeholder= "Enter title of Event "
-                            value = {this.state.event}
-                            onChange = {this.handleChange}
-                        />
-                        <h3> Date </h3>
-                        <Input 
-                            name= "date"
-                            type = "date"
-                            placeholder= "Enter time of event "
-                            value= {this.state.time}
-                            onChange= {this.handleChange}
-                        />  
-                        <h3> Start  Time </h3>
-                        <Input 
-                            name= "startTime"
-                            type = "time"
-                            placeholder= "Enter time of event "
-                            value= {this.state.startTime}
-                            onChange= {this.handleChange}
-                        />
-                        <h3> End Time </h3>
-                        <Input 
-                            name= "endTime"
-                            type = "time"
-                            placeholder= "Enter time of event "
-                            value= {this.state.endTime}
-                            onChange= {this.handleChange}
-                        />    
-                        <br/>
+                <FormControl p="50px" width="100%" onSubmit={this.onSubmit}>
+                    <FormLabel> Event Name </FormLabel>
+                    <Input
+                        name="event"
+                        type="text"
+                        placeholder="Enter title of Event "
+                        value={this.state.event}
+                        onChange={this.onChangeEvent}
+                    />
+                    <h3> Date </h3>
+                    <Input
+                        name="date"
+                        type="date"
+                        placeholder="Enter time of event "
+                        value={this.state.date}
+                        onChange={this.onChangeDate}
+                    />
+                    <h3> Start  Time </h3>
+                    <Input
+                        name="startTime"
+                        type="time"
+                        placeholder="Enter time of event "
+                        value={this.state.startTime}
+                        onChange={this.onChangestartTime}
+                    />
+                    <h3> End Time </h3>
+                    <Input
+                        name="endTime"
+                        type="time"
+                        placeholder="Enter time of event "
+                        value={this.state.endTime}
+                        onChange={this.onChangeendTime}
+                    />
+                    {/* <br/>
                         <Box> 
-                            <p>{this.state.event}</p>
+                            <p>{this.state.title}</p>
                             <br/>
                             <p>{this.state.date}</p>
                             <br/>
                             <p>{this.state.startTime}</p>
                             <br/>
                             <p>{this.state.endTime}</p>
-                        </Box>
-                        <br/>
-                        <Button 
-                        pos="absolute" 
+                        </Box> */}
+                    <br />
+                    <Button
+                        pos="absolute"
                         mt={4}
                         variantColor="teal"
-                        type="submit" 
-                        >
-                            Submit 
+                        type="submit"
+                        onClick={this.submitForm}
+                    >
+                        Submit
                         </Button>
-                   
-                </FormControl> 
+
+                </FormControl>
             </form>
-            
+
         )
     }
 }
